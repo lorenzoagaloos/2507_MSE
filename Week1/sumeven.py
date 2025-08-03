71a606b19dca68f22e7c91617c9b6ab15d6a0f40
lorenzoagaloos <lorenzo.agaloos@gmail.com>
2025-08-03 21:54:03 +1200

    Fixed Error
    Fixed an error that was being caused by an inadvertent extra comma within the code

--- Week1/sumeven.py
+++ Week1/sumeven.py
@@ -12,7 +12,7 @@ def SumEvenNumbers():
         output = 0
         i = 0
         while i <= n:
-            print(", ", i)
+            print(" ", i)
             output += i
             i += 2
         return output
